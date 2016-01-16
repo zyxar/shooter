de74ca77f648c95982b112934c200c49fffd36bc
Markus Tzoe <chou.marcus@gmail.com>
2016-01-16 22:12:09 +0800

    impl request

---
+++ request.go
@@ -0,0 +1,203 @@
+package shooter
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
+)
+
+var (
+	defaultConn struct {
+		*http.Client
+		sync.Mutex
+		timeout time.Duration
+	}
+	timeoutErr    = errors.New("request time out.")
+	timeoutMillis = 5000 * time.Millisecond
+)
+
+func init() {
+	defaultConn.timeout = timeoutMillis
+	defaultConn.Client = &http.Client{
+		Transport: &http.Transport{
+			Dial:  (&net.Dialer{Timeout: defaultConn.timeout}).Dial,
+			Proxy: http.ProxyFromEnvironment,
+		},
+	}
+}
+
+type subtitleDescription struct {
+	Desc  string
+	Delay int
+	Files []SubtitleFile
+}
+
+type SubtitleFile struct {
+	Ext      string
+	Link     string
+	FilmName *string `json:"-"`
+}
+
+func (this SubtitleFile) String() string {
+	v, _ := json.MarshalIndent(this, "", "  ")
+	return string(v)
+}
+
+func (this *SubtitleFile) Fetch() error {
+	req, err := http.NewRequest("GET", this.Link, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Origin", "http://shooter.cn/")
+	req.Header.Add("User-Agent", userAgent)
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Referer", "http://shooter.cn/")
+	resp, err := routine(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode/100 > 3 {
+		return errors.New(resp.Status)
+	}
+
+	var filename string
+	splits := strings.Split(resp.Header.Get("Content-Disposition"), "filename=")
+	if len(splits) > 1 {
+		if filepath.Ext(splits[1])[1:] != this.Ext {
+			return errors.New("filename extension not matched.")
+		}
+		filename = splits[1]
+		if this.FilmName == nil {
+			filmNameLen := len(splits[1]) - len(this.Ext)
+			filmName := splits[1][:filmNameLen]
+			this.FilmName = &filmName
+		}
+	} else if this.FilmName != nil {
+		filename = *this.FilmName + "." + this.Ext
+	} else {
+		return errors.New("filename not determined.")
+	}
+
+	var saveFile = func(filename string) error {
+		if _, err := os.Lstat(filename); os.IsNotExist(err) {
+			if file, err := os.Create(filename); err == nil {
+				fmt.Printf("Saving to %s...", filename)
+				if _, err = io.Copy(file, resp.Body); err != nil {
+					fmt.Printf("\t[%v]\n", err)
+					return err
+				}
+				fmt.Println("\t[OK]")
+				return nil
+			} else {
+				return err
+			}
+		}
+		return os.ErrExist
+	}
+	err = saveFile(filename)
+	i := 1
+	for err == os.ErrExist {
+		filename = fmt.Sprintf("%s-%d.%s", *this.FilmName, i, this.Ext)
+		err = saveFile(filename)
+		i++
+	}
+	return err
+}
+
+// [
+//   {
+//     "Desc": "",
+//     "Delay": 0,
+//     "Files": [
+//       {
+//         "Ext": "ass",
+//         "Link": "https://www.shooter.cn/api/subapi.php?fetch=MTQ1Mjk1MTI2Nnw5MFNuZ19EdGQ1Um5aQmtWa3JWSkFnbmVKTFNud1pmWHROMFhoVG1rc1loM3hRV1g5Mmg3bTJfbi1GX1hvNkdnZVBsVjQzbEFUMjZZNWNmeGVLUlozMUU3S1ZLSXduU1ZjdG4tN3J6dVlUWGkxT0ZnUVdleU1JcmdCams4R294SGZhbVoxaF9lTFkwckxLUT18tVUfkRy6VKi6-p3We7gbNo29gpZ2rJsdMLW6XLqRjhQ=&nonce=v%CE%1F%3A%40%C5%ED%F0%CF%AA%AC%3B%AF%15p%9D"
+//       }
+//     ]
+//   },
+//   {
+//     "Desc": "",
+//     "Delay": 0,
+//     "Files": [
+//       {
+//         "Ext": "srt",
+//         "Link": "https://www.shooter.cn/api/subapi.php?fetch=MTQ1Mjk1MTI2NnxnWkdHc0JDb3FpdnJXdkZ4STJsS2tPeVdfNFJVRkg0MkNQd1dKUjA5a2RyOV9oZFEteGp6RmVlZGpZT3A5c195U2VCNmw1LVczOUJaQ3BBY1NNbjk1Q3QtUGhoWlN0MmUtSkcwU1RTS3lpUDFsRDRlcW5kdWR0Tzc0T1Z1dlg5TTJqVi1UX0FyamdLVm1wcz18yAg8N3tsHJ9HJU8U7UElVBA7EBKnl20miy35fo7S1iY=&nonce=%C9%B3%00%0AzD%86%3C%EB%06D%F0%C10%03Y"
+//       }
+//     ]
+//   }
+// ]
+func Query(filehash, filename string) ([]SubtitleFile, error) {
+	v := url.Values{}
+	v.Set("filehash", filehash)
+	v.Set("pathinfo", filename)
+	v.Set("format", "json")
+	req, err := http.NewRequest("POST", "http://shooter.cn/api/subapi.php", strings.NewReader(v.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Origin", "http://shooter.cn/")
+	req.Header.Add("User-Agent", userAgent)
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Referer", "http://shooter.cn/")
+	resp, err := routine(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode/100 > 3 {
+		return nil, errors.New(resp.Status)
+	}
+	var desc []subtitleDescription
+	decoder := json.NewDecoder(resp.Body)
+	if err = decoder.Decode(&desc); err != nil {
+		return nil, errors.New("subtitles not found.")
+	}
+	subfiles := make([]SubtitleFile, 0, len(desc))
+	filmNameLen := len(filename) - len(filepath.Ext(filename))
+	filmName := filename[:filmNameLen]
+	for i, _ := range desc {
+		for j, _ := range desc[i].Files {
+			desc[i].Files[j].FilmName = &filmName
+			subfiles = append(subfiles, desc[i].Files[j])
+		}
+	}
+	return subfiles, nil
+}
+
+func routine(req *http.Request) (*http.Response, error) {
+	timeout := false
+retry:
+	defaultConn.Lock()
+	timer := time.AfterFunc(defaultConn.timeout, func() {
+		defaultConn.Client.Transport.(*http.Transport).CancelRequest(req)
+		timeout = true
+	})
+	resp, err := defaultConn.Do(req)
+	if timer != nil {
+		timer.Stop()
+	}
+	defaultConn.Unlock()
+	if err == io.EOF && !timeout {
+		// fmt.Fprintln(os.Stderr, "[RETRY]")
+		goto retry
+	}
+	if timeout {
+		err = timeoutErr
+	}
+	return resp, err
+}
---
+++ request_test.go
@@ -0,0 +1,27 @@
+package shooter
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRequest(t *testing.T) {
+	filehash := "66781fc73341bf357500505ad7de1ede;e75e7b0e54b37e3ca511523314e3f1e2;454a5bcb53654a08b2345a606e0cafe6;3f2af6eab10caa9909c0a54652400dd9"
+	filename := "Eva.2011.720p.BluRay.x264-DON.mkv"
+	files, err := Query(filehash, filename)
+	if err != nil {
+		t.Error(err)
+	}
+	chs := make(chan error, len(files))
+	for i := range files {
+		go func(i int) {
+			chs <- files[i].Fetch()
+		}(i)
+	}
+	for i := 0; i < len(files); i++ {
+		err = <-chs
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
